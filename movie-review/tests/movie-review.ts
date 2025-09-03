446dd26db224bcd8b8f93a8f609f9e562e09b2a3
Utkarsh Uday <utkarshsahil629@gmail.com>
2025-09-03 20:46:16 +0530

    tests modified

--- movie-review/tests/movie-review.ts
+++ movie-review/tests/movie-review.ts
@@ -2,6 +2,12 @@ import * as anchor from '@coral-xyz/anchor';
 import { Program } from '@coral-xyz/anchor';
 import { MovieReview } from '../target/types/movie_review';
 import { expect } from 'chai';
+import {
+  getAccount,
+  getAssociatedTokenAddress,
+  getMint,
+  TOKEN_2022_PROGRAM_ID,
+} from '@solana/spl-token';
 
 describe('movie-review', () => {
   const provider = anchor.AnchorProvider.env();
@@ -20,18 +26,62 @@ describe('movie-review', () => {
     program.programId
   );
 
+  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from('mint')],
+    program.programId
+  );
+
+  it('Mint initialized!', async () => {
+    const tx = await program.methods
+      .createMint()
+      .accounts({
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
+      .signers([user.payer])
+      .rpc({ commitment: 'confirmed' });
+    console.log('Your transaction signature', tx);
+
+    const mintAccount = await getMint(
+      program.provider.connection,
+      mint,
+      'confirmed',
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    console.log('Mint Account', mintAccount);
+  });
+
   it('Movie review added', async () => {
     const tx = await program.methods
       .addMovieReview(movie.title, movie.rating, movie.description)
-      .accounts({ user: user.publicKey })
+      .accounts({
+        user: user.publicKey,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
+      })
       .signers([user.payer])
-      .rpc();
+      .rpc({ commitment: 'confirmed' });
+
+    const associatedTokenAccount = await getAssociatedTokenAddress(
+      mint,
+      program.provider.publicKey,
+      false,
+      TOKEN_2022_PROGRAM_ID
+    );
+
+    const tokenAccount = await getAccount(
+      program.provider.connection,
+      associatedTokenAccount,
+      'confirmed',
+      TOKEN_2022_PROGRAM_ID
+    );
+    console.log('Token Account', tokenAccount);
 
     const account = await program.account.movieReviewAccount.fetch(moviePda);
     expect(account.title).to.equal(movie.title);
     expect(account.rating).to.equal(movie.rating);
     expect(account.description).to.equal(movie.description);
     expect(account.reviewer.toBase58()).to.equal(user.publicKey.toBase58());
+    expect(Number(tokenAccount.amount)).to.equal(10 * Math.pow(10, 6));
 
     console.log('Your transaction signature', tx);
   });
